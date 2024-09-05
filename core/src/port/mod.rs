5c339afdfe01a28dbfb626be4950b19d9e0f1a9a
Jackson Sippe <jasi7701@colorado.edu>
2024-09-05 17:45:51 -0400

    Assign RSS offload type with NIC supported values

--- core/src/port/mod.rs
+++ core/src/port/mod.rs
@@ -295,7 +295,8 @@ impl Port {
             port_conf.rx_adv_conf.rss_conf.rss_key = SYMMETRIC_RSS_KEY.as_ptr() as *mut u8;
             port_conf.rx_adv_conf.rss_conf.rss_key_len = RSS_KEY_LEN as u8;
             port_conf.rx_adv_conf.rss_conf.rss_hf =
-                (dpdk::ETH_RSS_IP | dpdk::ETH_RSS_TCP | dpdk::ETH_RSS_UDP) as u64;
+                (dpdk::ETH_RSS_IP | dpdk::ETH_RSS_TCP | dpdk::ETH_RSS_UDP) as u64
+                    & dev_info.flow_type_rss_offloads;
         }
 
         let max_rx_pkt_len = mtu_to_max_frame_len(mtu as u32);
