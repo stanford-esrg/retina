709614d8adab2a9d1f61722343a890cc0660bcc7
Diana Qing <dianaqing@berkeley.edu>
2024-11-21 12:18:49 -0800

    test ssh callback

--- examples/test_ssh/src/main.rs
+++ examples/test_ssh/src/main.rs
@@ -1,30 +1,46 @@
-use retina_core::config::{load_config, Runtime};
-use retina_core::subscription::Connection;
+use retina_core::{config::default_config, Runtime};
+// use retina_core::subscription::Connection;
 use retina_datatypes::{ConnRecord, SshTransaction};
 use retina_filtergen::{filter, retina_main};
 
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
-use anyhow::Result;
-use clap::Parser;
+// use clap::Parser;
 
-#[derive(Parser, Debug)]
-struct Args {
-    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
-    config: PathBuf,
-}
+// #[derive(Parser, Debug)]
+// struct Args {
+//     #[clap(short, long, parse(from_os_str), value_name = "FILE")]
+//     config: PathBuf,
+// }
+
+// #[filter("ssh")]
+// fn frame_cb(mbuf: &ZcFrame) {
+//     let contents = mbuf.data();
+//     println!("mbuf data: {:x?}", contents);
+// }
+
+// #[filter("ssh")]
+// fn five_tuple_cb(five_tuple: &FiveTuple, pkts: &PktCount) {
+//     let dst_port = five_tuple.resp.port();
+//     let src_port = five_tuple.orig.port();
+//     println!("dst_port: {:?}", dst_port);
+//     println!("src_port: {:?}", src_port);
+// }
 
 #[filter("ssh")]
-fn ssh_cb(ssh: &SshTransaction, five_tuple: &FiveTuple) {
-
+fn ssh_cb(ssh: &SshTransaction, conn_record: &ConnRecord) {
+    println!(
+        "SSH protocol version: {}, conn. metrics: {:?}",
+        ssh.protocol_version(),
+        conn_record
+    );
 }
 
+#[retina_main(1)]
 fn main() {
     let args = Args::parse();
     let config = load_config(&args.config);
 
     let mut runtime: Runtime<SubscribedWrapper> = Runtime::new(config, filter).unwrap();
     runtime.run();
-
-    Ok(())
 }
