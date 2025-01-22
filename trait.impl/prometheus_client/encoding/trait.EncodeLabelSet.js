638e58d49ce883188ffd6ed374e470b854433eed
thearossman <thearossman@users.noreply.github.com>
2025-01-22 20:29:11 +0000

    deploy: c3d5badfccf89699072e172652c764f771be01fa

---
+++ trait.impl/prometheus_client/encoding/trait.EncodeLabelSet.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"retina_core":[["impl EncodeLabelSet for <a class=\"struct\" href=\"retina_core/lcore/struct.CoreId.html\" title=\"struct retina_core::lcore::CoreId\">CoreId</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
