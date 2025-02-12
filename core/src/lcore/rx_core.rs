d1e1aa9f39f3bab92d1e0991cccd009605670ffe
hkalbasi <hamidrezakalbasi@protonmail.com>
2025-02-12 18:54:25 +0330

    Update timer and metrics based on total cycles instead of idle cycles

--- core/src/lcore/rx_core.rs
+++ core/src/lcore/rx_core.rs
@@ -103,17 +103,17 @@ where
                 let mbufs: Vec<Mbuf> = self.rx_burst(rxqueue, 32);
                 if mbufs.is_empty() {
                     IDLE_CYCLES.inc();
-
-                    if IDLE_CYCLES.get() & 1023 == 512 {
-                        now = Instant::now();
-                    }
-
-                    #[cfg(feature = "prometheus")]
-                    if IDLE_CYCLES.get() & 1023 == 0 && self.is_prometheus_enabled {
-                        crate::stats::update_thread_local_stats(self.id);
-                    }
                 }
+
                 TOTAL_CYCLES.inc();
+                if TOTAL_CYCLES.get() & 1023 == 512 {
+                    now = Instant::now();
+                }
+                #[cfg(feature = "prometheus")]
+                if TOTAL_CYCLES.get() & 1023 == 0 && self.is_prometheus_enabled {
+                    crate::stats::update_thread_local_stats(self.id);
+                }
+
                 for mbuf in mbufs.into_iter() {
                     // log::debug!("{:#?}", mbuf);
                     // log::debug!("Mark: {}", mbuf.mark());
