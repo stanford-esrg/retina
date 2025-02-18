0a8b1a39a5d2286a44a66fd5d43478cff66ceca5
Diana Qing <dianawqing@gmail.com>
2025-02-18 20:38:41 +0000

    remove print statements from example

--- examples/log_ssh/src/main.rs
+++ examples/log_ssh/src/main.rs
@@ -40,7 +40,6 @@ fn ssh_byte_match_cb(ssh: &SshHandshake) {
 // check if contains b"OpenSSH"
 #[filter("ssh.software_version_ctos contains |4F 70 65 6E 53 53 48|")]
 fn ssh_contains_bytes_cb(ssh: &SshHandshake) {
-    println!("contains bytes: ssh.software_version_ctos: {}", ssh.software_version_ctos());
     if let Ok(serialized) = serde_json::to_string(&ssh) {
         let mut wtr = file.lock().unwrap();
         wtr.write_all(serialized.as_bytes()).unwrap();
@@ -50,7 +49,6 @@ fn ssh_contains_bytes_cb(ssh: &SshHandshake) {
 
 #[filter("ssh.software_version_ctos contains 'OpenSSH'")]
 fn ssh_contains_str_cb(ssh: &SshHandshake) {
-    println!("contains str: ssh.software_version_ctos: {}", ssh.software_version_ctos());
     if let Ok(serialized) = serde_json::to_string(&ssh) {
         let mut wtr = file.lock().unwrap();
         wtr.write_all(serialized.as_bytes()).unwrap();
