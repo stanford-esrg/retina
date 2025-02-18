82cf7c7d6f0051792be05c3c08f9eb72c0576286
Diana Qing <dianawqing@gmail.com>
2025-02-18 20:31:47 +0000

    remove commented out example

--- examples/log_ssh/src/main.rs
+++ examples/log_ssh/src/main.rs
@@ -28,15 +28,6 @@ struct Args {
     outfile: PathBuf,
 }
 
-// #[filter("ssh.software_version_ctos ~ '^OpenSSH_[0-9]+\\.[0-9].*$'")]
-// fn ssh_cb(ssh: &SshHandshake) {
-//     if let Ok(serialized) = serde_json::to_string(&ssh) {
-//         let mut wtr = file.lock().unwrap();
-//         wtr.write_all(serialized.as_bytes()).unwrap();
-//         wtr.write_all(b"\n").unwrap();
-//     }
-// }
-
 #[filter("ssh.protocol_version_ctos = |32 2E 30|")]
 fn ssh_byte_match_cb(ssh: &SshHandshake) {
     if let Ok(serialized) = serde_json::to_string(&ssh) {
