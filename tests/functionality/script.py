817d2b227b56578b7c182a623bfb8a2c5ffc5b16
AliyaAlsafa1 <aliya.m.safa@gmail.com>
2025-05-14 20:09:21 +0000

    Updated script.py to handle test cases

--- tests/functionality/script.py
+++ tests/functionality/script.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
+import os
 
-app_directory = "../retina/target/debug/test_app"
+if len(sys.argv) < 4:
+    print("Please pass in the testing app name, input file, and pcap file as arguments!")
+    print("Format: [app_name] [input file path] [pcap file path]")
+    sys.exit(1)
 
-args = []
+app_name = sys.argv[1]
+input_file = sys.argv[2]
+pcap_file = sys.argv[3] # path to config file, reads from pcap file use offline pcap
+app_directory = os.path.join("..", "retina", "target", "debug", app_name)
+
+args = [input_file, pcap_file]
 
 try:
     result = subprocess.run(
-            [app_directory] + args,
+            [app_directory],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
     )
     print("Output:\n", result.stdout)
+
+# Check if output matches input file format / values
+# Print whether passed or not
+
 except subprocess.CalledProcessError as e:
     print("Error:\n", e.stderr)
--- tests/functionality/test_app/src/main.rs
+++ tests/functionality/test_app/src/main.rs
@@ -2,6 +2,16 @@ use retina_core::{config::default_config, Runtime};
 use retina_datatypes::{ConnRecord, DnsTransaction, TlsHandshake};
 use retina_filtergen::{filter, retina_main};
 
+// Goals for application:
+// Take in a packet capture file... run retina application using data from that file
+// Application should count amount of times callback is invoked
+// Also include matching of five_tuples, etc. (will be in main application I'm designing so logic is the same)
+// Format results the same as input file (expected format)
+// Output passed/not passed and results in terminal and also in a file? (appname_configfilename_output.txt)
+// Do we want to be able to save output multiple times? (multiple files like test_app_output_1.txt, etc.)
+
+
+
 #[filter("tls")]
 fn tls_cb(tls: &TlsHandshake, conn_record: &ConnRecord) {
     println!("Tls SNI: {}, conn. metrics: {:?}", tls.sni(), conn_record);
@@ -21,5 +31,4 @@ fn main() {
     let config = default_config();
     let mut runtime: Runtime<SubscribedWrapper> = Runtime::new(config, filter).unwrap();
     runtime.run();
-    // println!("Hello world from Rust!");
 }
